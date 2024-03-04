e546d5e5c11460f0f56971c7db7b060d73cf2598
Egzamin <Egzamin@TE-CZE-D-01S012>
2024-03-04 11:49:09 +0100

    Dodaj pliki projektów.

---
+++ Zadanie1/Magazyn.cs
@@ -0,0 +1,56 @@
+﻿using Zadanie1;
+
+using System;
+using System.Collections.Generic;
+public class Produkt
+{
+    public string Nazwa { get; set; }
+    public decimal Cena { get; set; }
+    public int Ilość { get; set; }
+
+    public Produkt(string nazwa, decimal cena, int ilość)
+    {
+        Nazwa = nazwa;
+        Cena = cena;
+        Ilość = ilość;
+    }
+
+    public void WyświetlInformacje()
+    {
+        Console.WriteLine($"Nazwa: {Nazwa}, Cena: {Cena}, Ilość: {Ilość}");
+    }
+
+
+}
+
+public class Magazyn
+{
+    public string Nazwa { get; set; }
+    public List<Produkt> Produkty { get; set; }
+
+    public Magazyn(string nazwa)
+    {
+        Nazwa = nazwa;
+        Produkty = new List<Produkt>();
+    }
+
+    public void DodajProdukt(Produkt produkt)
+    {
+        Produkty.Add(produkt);
+    }
+
+    public void WyświetlProdukty()
+    {
+        foreach (var produkt in Produkty)
+        {
+            produkt.WyświetlInformacje();
+        }
+    }
+
+    public void SortujProdukty()
+    {
+        Produkty.Sort(); 
+    }
+}
+
+
---
+++ Zadanie1/Produkt.cs
@@ -0,0 +1,45 @@
+﻿namespace Zadanie1;
+using System;
+using System.Collections.Generic;
+
+public class Produkt
+{
+    public string Nazwa { get; set; }
+    public decimal Cena { get; set; }
+    public int Ilość { get; set; }
+
+    public Produkt(string nazwa, decimal cena, int ilość)
+    {
+        Nazwa = nazwa;
+        Cena = cena;
+        Ilość = ilość;
+    }
+
+    public void WyświetlInformacje()
+    {
+        Console.WriteLine($"Nazwa: {Nazwa}, Cena: {Cena}, Ilość: {Ilość}");
+    }
+
+  
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Produkt produkt1 = new Produkt("Zegarek",1333 , 70);
+        Produkt produkt2 = new Produkt("Fifa",100, 40);
+        Produkt produkt3 = new Produkt("Telefon", 3500, 8);
+
+        
+        produkt1.WyświetlInformacje();
+        produkt2.WyświetlInformacje();
+        produkt3.WyświetlInformacje();
+
+        
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+        Console.ReadKey();
+    }
+}
